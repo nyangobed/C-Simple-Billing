e2595e9caf04cd43660632c4f8577463a0d47d6c
nyangobed <obednyangate@gmail.com>
2019-05-15 16:41:01 +0300

    first commit

---
+++ cmake-build-debug/CMakeFiles/Billing.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Billing.dir/main.c.obj"
+  "Billing.pdb"
+  "Billing.exe"
+  "Billing.exe.manifest"
+  "libBilling.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Billing.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
